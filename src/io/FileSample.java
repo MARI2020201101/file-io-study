d1e5a041e62af7f05b90bc740bfba717d037d65f
MARI <mari20201101@gmail.com>
2021-08-27 22:15:10 +0900

    Check lastmodified date of file

--- src/io/FileSample.java
+++ src/io/FileSample.java
@@ -1,13 +1,16 @@
 package io;
 
 import java.io.File;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class FileSample {
     public static void main(String[] args) {
         FileSample sample = new FileSample();
         String pathName = "C:\\study\\file-io-study\\sample.txt";
      //   sample.checkpath(pathName);
-        sample.makeDir(pathName);
+     //   sample.makeDir(pathName);
+        sample.lastModified(pathName);
     }
 
     private void checkpath(String pathName) {
@@ -21,6 +24,10 @@ public class FileSample {
             boolean mkdir =file.mkdir();
             System.out.println(pathName +" mkdir result: "+ mkdir);
         }
+    }
 
+    private void lastModified(String pathName){
+        File file = new File(pathName);
+        System.out.println("file.lastModified() : "+ new Date(file.lastModified()));
     }
 }
