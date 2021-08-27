d5188b92de48b88edaea35205d67d142ddf8f614
MARI <mari20201101@gmail.com>
2021-08-27 22:11:42 +0900

    Create mkdir method

--- src/io/FileSample.java
+++ src/io/FileSample.java
@@ -5,12 +5,22 @@ import java.io.File;
 public class FileSample {
     public static void main(String[] args) {
         FileSample sample = new FileSample();
-        String pathName = "C:\\study\\file-io-study";
-        sample.checkpath(pathName);
+        String pathName = "C:\\study\\file-io-study\\sample.txt";
+     //   sample.checkpath(pathName);
+        sample.makeDir(pathName);
     }
 
     private void checkpath(String pathName) {
         File file = new File(pathName);
         System.out.println(pathName + " is Exist> : "+ file.exists());
+    }
+
+    private void makeDir(String pathName){
+        File file = new File(pathName);
+        if(!file.exists()){
+            boolean mkdir =file.mkdir();
+            System.out.println(pathName +" mkdir result: "+ mkdir);
+        }
+
     }
 }
